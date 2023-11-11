6d77988812396d74bf245f02c7a6d47b1a3f8caf
ivanamin <ivanamin@student.21-school.ru>
2023-11-11 15:42:23 +0300

    testing

---
+++ src/pages/api/auth/signin.ts
@@ -0,0 +1,54 @@
+import type { APIRoute } from "astro";
+import { supabase } from "~/lib/supabase";
+import type { Provider } from "@supabase/supabase-js";
+
+export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+    console.log(request);
+    const formData = await request.formData();
+    const email = formData.get("email")?.toString();
+    const password = formData.get("password")?.toString();
+    const provider = formData.get("provider")?.toString();
+  
+    if (provider) {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: provider as Provider,
+        options: {
+          redirectTo: import.meta.env.DEV
+            ? "https://4321-luwakiotsec-luwaklabfro-fqxlildt2oh.ws-eu106.gitpod.io/api/auth/callback"
+            : "https://4321-luwakiotsec-luwaklabfro-fqxlildt2oh.ws-eu106.gitpod.io/api/auth/callback",
+        },
+      });
+  
+      if (error) {
+        return new Response(error.message, { status: 500 });
+      }
+      return redirect(data.url);
+    }
+  
+    if (!email || !password) {
+      return new Response("Email and password are required", { status: 400 });
+    }
+  
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+  
+    if (error) {
+      return new Response(error.message, { status: 500 });
+    }
+  
+    const { access_token, refresh_token } = data.session;
+    cookies.set("sb-access-token", access_token, {
+      sameSite: "strict",
+      path: "/",
+      secure: true,
+    });
+    cookies.set("sb-refresh-token", refresh_token, {
+      sameSite: "strict",
+      path: "/",
+      secure: true,
+    });
+    
+    return redirect("/dashboard");
+  };
